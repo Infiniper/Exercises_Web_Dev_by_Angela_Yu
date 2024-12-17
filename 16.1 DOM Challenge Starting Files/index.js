6f4d66790493ebedf6e3eec2e4561cafb279c7b1
Infiniper <infiniper@gmail.com>
2024-12-18 00:30:52 +0530

    Started Module 16.

---
+++ 16.1 DOM Challenge Starting Files/index.js
@@ -0,0 +1,3 @@
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML="Angela Yu";
+// This will not work:
+// document.firstElementChild.lastElementChild.lastElementChild.lastElementChild.innerHTML="Angela Yu";
