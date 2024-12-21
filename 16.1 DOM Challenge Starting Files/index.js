732b7f8924890b3240d1c12d51d1ad2ce208dbf2
Infiniper <infiniper@gmail.com>
2024-12-21 19:27:14 +0530

    Completed DOM.

--- 16.1 DOM Challenge Starting Files/index.js
+++ 16.1 DOM Challenge Starting Files/index.js
@@ -1,3 +1,6 @@
 document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML="Angela Yu";
 // This will not work:
 // document.firstElementChild.lastElementChild.lastElementChild.lastElementChild.innerHTML="Angela Yu";
+
+//Changing the background color of the click me button to Yellow
+document.querySelector("button").style.backgroundColor="Yellow";
