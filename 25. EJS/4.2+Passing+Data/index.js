70dde66e4bf76b0705b1f7bd0abc9b317814992b
Infiniper <infiniper@gmail.com>
2025-01-22 23:32:44 +0530

    EJS

--- 25. EJS/4.2+Passing+Data/index.js
+++ 25. EJS/4.2+Passing+Data/index.js
@@ -1,14 +1,26 @@
 import express from "express";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const fName= req.body["fName"];
+  const lName= req.body["lName"];
+  const length= (fName+lName).length;
+  res.render("index.ejs", {
+    length:length,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
